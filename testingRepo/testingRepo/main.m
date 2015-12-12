6738fa7a2f66418c2d6842508470dbff08d1b52c
Rohit Singh <rohitsr1991@gmail.com>
2015-12-12 19:00:36 +0530

    first project commit

---
+++ testingRepo/testingRepo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testingRepo
+//
+//  Created by Rohit Raghuvanshi on 12/12/15.
+//  Copyright © 2015 nsimpl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
