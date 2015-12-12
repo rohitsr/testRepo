085a547b94a47bf53394f32e953181982910c3f5
Rohit Singh <rohitsr1991@gmail.com>
2015-12-12 19:19:48 +0530

    fourth commit

---
+++ testingRepo3/testingRepo3/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  testingRepo3
+//
+//  Created by Rohit Raghuvanshi on 12/12/15.
+//  Copyright © 2015 nsimpl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ testingRepo3/testingRepo3Tests/testingRepo3Tests.m
@@ -0,0 +1,39 @@
+//
+//  testingRepo3Tests.m
+//  testingRepo3Tests
+//
+//  Created by Rohit Raghuvanshi on 12/12/15.
+//  Copyright © 2015 nsimpl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testingRepo3Tests : XCTestCase
+
+@end
+
+@implementation testingRepo3Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
